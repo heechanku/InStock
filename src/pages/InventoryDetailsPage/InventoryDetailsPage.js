59d1c9901cd8ba718ecbc6e4e283d4d572e3d284
Damien Switzer <damienswitzer@hotmail.com>
2023-04-26 16:40:46 -0700

    Integrate api as data source

--- src/pages/InventoryDetailsPage/InventoryDetailsPage.js
+++ src/pages/InventoryDetailsPage/InventoryDetailsPage.js
@@ -3,34 +3,44 @@ import "./InventoryDetailsPage.scss";
 import backArrow from '../../assets/Icons/arrow_back-24px.svg';
 import editIcon from '../../assets/Icons/edit-24px.svg';
 import InventoryDetails from "../../components/InventoryDetails/InventoryDetails";
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
-
-const testItem = {
-  id: '9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3',
-  warehouse_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
-  item_name: 'Television',
-  description:
-    'This 50", 4K LED TV provides a crystal-clear picture and vivid colors.',
-  category: 'Electronics',
-  status: 'In Stock',
-  quantity: 500,
-};
+const baseUrl = process.env.REACT_APP_BASE_URL ?? "http://localhost:5050/api";
 
 function InventoryDetailsPage() {
   const { id } = useParams();
+  const [item, setItem] = useState(null);
 
-  //TEMP
-  const item = testItem;
-  //TEMP
+  console.log(`${baseUrl}/inventories/${id}`);
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/inventories/${id}`)
+      .then(response => {
+        setItem(response.data);
+        console.log(response.data);
+      }).catch(error => {
+        console.error(error);
+        alert(error);
+      })
+  }, []);
+
+  //TEMP (Awaiting correction to api response)
   const warehouseName = "Manhattan";
+
+  const { item_name, description, category, status, quantity } = item ?? {};
 
   return (
     <main className="inventory-details-page">
       <div className="inventory-details-page__header">
-        <h1 className="inventory-details-page__title"><button className="inventory-details-page__back-button"><img className="inventory-details-page__back-icon" src={backArrow} alt="back" /></button>{item.item_name}</h1>
+        <h1 className="inventory-details-page__title">
+          <button className="inventory-details-page__back-button"><img className="inventory-details-page__back-icon" src={backArrow} alt="back" /></button>
+          {item_name ?? "Loading..."}
+        </h1>
         <button className="inventory-details-page__edit-button"><img className="inventory-details-page__edit-icon" src={editIcon} alt="edit" /><span className="inventory-details-page__edit-text">Edit</span></button>
       </div>
-      <InventoryDetails description={item.description} category={item.category} status={item.status} quantity={item.quantity} warehouseName={warehouseName} />
+      <InventoryDetails description={description} category={category} status={status} quantity={quantity} warehouseName={warehouseName} />
     </main>
   );
 }
