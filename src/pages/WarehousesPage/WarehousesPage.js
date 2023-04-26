aa7af0f36aedf65bd63ad17c6c6511af63d32808
Damien Switzer <damienswitzer@hotmail.com>
2023-04-25 17:44:42 -0700

    Design warehouses page

--- src/pages/WarehousesPage/WarehousesPage.js
+++ src/pages/WarehousesPage/WarehousesPage.js
@@ -1,9 +1,15 @@
 import "./WarehousesPage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import searchIcon from "../../assets/Icons/search-24px.svg";
 
 function WarehousesPage() {
   return (
     <main className="warehouses-page">
+      <div className="warehouses-page__header">
+        <h1 className="warehouses-page__title">Warehouses</h1>
+        <input type="search" name="search" className="warehouses-page__search-field" placeholder="Search..." />
+        <button className="warehouses-page__add-button">+ Add New Warehouse</button>
+      </div>
       <WarehouseList />
     </main>
   );
