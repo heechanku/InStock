eee7ff3f261301aa123793620afefd247dcdf756
Heechan Ku <hku0202@gmail.com>
2023-04-26 20:29:03 -0400

    added inventory list

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -4,26 +4,26 @@ import sortIcon from "../../assets/Icons/sort-24px.svg";
 
 //Test Data
 const inventoryData = [
-    {
-        id: '9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3',
-        inventory_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
-        item_name: 'Television',
-        description:
-          'This 50", 4K LED TV provides a crystal-clear picture and vivid colors.',
-        category: 'Electronics',
-        status: 'In Stock',
-        quantity: 500,
-      },
-      {
-        id: '83433026-ca32-4c6d-bd86-a39ee8b7303e',
-        inventory_id: '2922c286-16cd-4d43-ab98-c79f698aeab0',
-        item_name: 'Gym Bag',
-        description:
-          'Made out of military-grade synthetic materials, this gym bag is highly durable, water resistant, and easy to clean.',
-        category: 'Gear',
-        status: 'Out of Stock',
-        quantity: 0,
-      }
+  {
+    id: "9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3",
+    inventory_id: "2922c286-16cd-4d43-ab98-c79f698aeab0",
+    item_name: "Television",
+    description:
+      'This 50", 4K LED TV provides a crystal-clear picture and vivid colors.',
+    category: "Electronics",
+    status: "In Stock",
+    quantity: 500,
+  },
+  {
+    id: "83433026-ca32-4c6d-bd86-a39ee8b7303e",
+    inventory_id: "2922c286-16cd-4d43-ab98-c79f698aeab0",
+    item_name: "Gym Bag",
+    description:
+      "Made out of military-grade synthetic materials, this gym bag is highly durable, water resistant, and easy to clean.",
+    category: "Gear",
+    status: "Out of Stock",
+    quantity: 0,
+  },
 ];
 
 function InventoryList() {
@@ -37,20 +37,22 @@ function InventoryList() {
     <div className="inventory-list">
       <div className="inventory-list__header-row">
         <div className="inventory-list__header-item inventory-list__header-item--name">
-          INVENTORY ITEM <img className="inventory-list__icon" src={sortIcon} alt="sort" />
+          INVENTORY ITEM{" "}
+          <img className="inventory-list__icon" src={sortIcon} alt="sort" />
         </div>
         <div className="inventory-list__header-item inventory-list__header-item--category">
-          CATEGORY <img className="inventory-list__icon" src={sortIcon} alt="sort" />
+          CATEGORY{" "}
+          <img className="inventory-list__icon" src={sortIcon} alt="sort" />
         </div>
         <div className="inventory-list__header-item inventory-list__header-item--status">
-          STATUS <img className="inventory-list__icon" src={sortIcon} alt="sort" />
+          STATUS{" "}
+          <img className="inventory-list__icon" src={sortIcon} alt="sort" />
         </div>
         <div className="inventory-list__header-item inventory-list__header-item--quantity">
-          QUANTITY <img className="inventory-list__icon" src={sortIcon} alt="sort" />
+          QUANTITY{" "}
+          <img className="inventory-list__icon" src={sortIcon} alt="sort" />
         </div>
-        <div className="inventory-list__header-item">
-          ACTIONS
-        </div>
+        <div className="inventory-list__header-item">ACTIONS</div>
       </div>
       <div className="inventory-list__body">
         <div className="inventory-list__row">
--- src/components/InventoryRow/InventoryRow.js
+++ src/components/InventoryRow/InventoryRow.js
@@ -4,15 +4,20 @@ import chevronRightIcon from "../../assets/Icons/chevron_right-24px.svg";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
 
-function InventoryRow({ id, name, category, status, quantity, onDelete, onEdit }) {
-
-    if (status === "In Stock"){
-       status = "IN STOCK"
-       
-
-    }else {
-        status = "OUT OF STOCK"
-    }
+function InventoryRow({
+  id,
+  name,
+  category,
+  status,
+  quantity,
+  onDelete,
+  onEdit,
+}) {
+  if (status === "In Stock") {
+    status = "IN STOCK";
+  } else {
+    status = "OUT OF STOCK";
+  }
   return (
     <div className="inventory-row">
       <div className="inventory-row__col inventory-row__col--name">
@@ -20,34 +25,42 @@ function InventoryRow({ id, name, category, status, quantity, onDelete, onEdit }
         <Link className="inventory-row__link" to={`/${id}`}>
           {name}
         </Link>
-        <img className="inventory-row__icon" src={chevronRightIcon} alt="open" />
+        <img
+          className="inventory-row__icon"
+          src={chevronRightIcon}
+          alt="open"
+        />
       </div>
       <div className="inventory-row__col inventory-row__col--category">
         <h4 className="inventory-row__cell-header">CATEGORY</h4>
         {category}
       </div>
       <div className="inventory-row__col inventory-row__col--status">
-        
         <h4 className="inventory-row__cell-header">STATUS</h4>
-        
-        <div className= {`inventory-row__col--status--${status === "IN STOCK" ? "green" : "red"}`}>
-        {status}
+
+        <div
+          className={`inventory-row__col--status--${
+            status === "IN STOCK" ? "green" : "red"
+          }`}
+        >
+          {status}
         </div>
       </div>
 
       <div className="inventory-row__col inventory-row__col--quantity">
         <h4 className="inventory-row__cell-header">QUANTITY</h4>
         <div className="inventory-row__contact-info">
-          <p className="inventory-row__quantity">
-            {quantity}
-          </p>
+          <p className="inventory-row__quantity">{quantity}</p>
         </div>
       </div>
       <div className=" inventory-row__col--actions">
         <button className="inventory-row__icon-button" onClick={onDelete}>
           <img className="inventory-row__icon" src={deleteIcon} alt="delete" />
         </button>
-        <button className="inventory-row__icon-button inventory-row__icon-button--edit" onClick={onEdit}>
+        <button
+          className="inventory-row__icon-button inventory-row__icon-button--edit"
+          onClick={onEdit}
+        >
           <img className="inventory-row__icon" src={editIcon} alt="edit" />
         </button>
       </div>
