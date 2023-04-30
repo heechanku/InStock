7c7ea243e999ed9bca4dc9f4a942e0a5e9bbcb42
Damien Switzer <damienswitzer@hotmail.com>
2023-04-29 18:48:19 -0700

    Add error state to fields, correct spacing to match mockups

--- src/components/NewInventoryItem/NewInventoryItem.js
+++ src/components/NewInventoryItem/NewInventoryItem.js
@@ -1,8 +1,9 @@
 import { useRef, useState, useEffect } from 'react';
 import './NewInventoryItem.scss'
-import { Link,  useNavigate  } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import backArrow from '../../assets/Icons/arrow_back-24px.svg'
+import errorIcon from '../../assets/Icons/error-24px.svg';
 
 
 export default function NewInventoryItem() {
@@ -16,9 +17,10 @@ export default function NewInventoryItem() {
     const [status, setStatus] = useState("");
     const [warehouses, setWarehouses] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [errors, setErrors] = useState({ itemName: false, description: false, quantity: false, selectedCategory: false, warehouse: false, status: false });
     const navigate = useNavigate();
 
-  
+
     const handleCancel = () => {
         navigate(`/inventory`);
 
@@ -26,7 +28,7 @@ export default function NewInventoryItem() {
 
     function handleStatusChange(event) {
         setStatus(event.target.value);
-      }
+    }
 
     //Onload get warehouses and categories
     useEffect(() => {
@@ -48,8 +50,8 @@ export default function NewInventoryItem() {
                 console.error(error);
                 alert("Error while retrieving categories");
             });
-        }, [])
-       
+    }, [])
+
 
     const handleWarehouseChange = (e) => {
         const warehouseId = e.target.value;
@@ -61,24 +63,45 @@ export default function NewInventoryItem() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Check that all fields are filled out
-        if (
-            itemName.current.value.trim() === "" ||
-            description.current.value.trim() === "" ||
-            selectedCategory === "" ||
-            status === "" ||
-            quantity=== "" || 
-            selectedWarehouse === null
-        ) {
+
+        let newErrors = { ...errors };
+
+        newErrors.itemName = itemName.current.value.trim() === "";
+        newErrors.description = description.current.value.trim() === "";
+        newErrors.category = selectedCategory === "";
+        newErrors.status = status === "";
+        newErrors.quantity = quantity === "";
+        newErrors.warehouse = selectedWarehouse === "";
+
+        setErrors(newErrors);
+
+        if (newErrors.itemName || newErrors.description || newErrors.category || newErrors.status || newErrors.quantity) {
             alert("Please fill out all fields.");
             return;
         }
+
+
+        // // Check that all fields are filled out
+        // if (
+        //     itemName.current.value.trim() === "" ||
+        //     description.current.value.trim() === "" ||
+        //     selectedCategory === "" ||
+        //     status === "" ||
+        //     quantity=== "" || 
+        //     selectedWarehouse === null
+        // ) {
+        //     alert("Please fill out all fields.");
+
+
+
+        //     return;
+        // }
 
         // check that quantity input is a postive number
         const quantityValue = quantity.current.value;
         if (isNaN(quantityValue) || quantityValue < 0) {
             alert("Quantity must be a positive number..");
-            
+
         }
         //create new item
         const newItem = {
@@ -91,18 +114,18 @@ export default function NewInventoryItem() {
         }
         //send new item to server
         axios.post(`http://localhost:5050/api/inventories`, newItem)
-        .catch(error => {
-            alert(error);
-            console.error(error);
-        });
+            .catch(error => {
+                alert(error);
+                console.error(error);
+            });
     }
 
 
     return (
         <main>
             <div className="inventory-item__header">
-        <h1 className="inventory-item__title">
-          <Link to="/"><button className="inventory-item__back-button"><img className="inventory-item__back-icon" src={backArrow} alt="back" /></button></Link>{"Add New Inventory Item"}</h1>
+                <h1 className="inventory-item__title">
+                    <Link to="/"><button className="inventory-item__back-button"><img className="inventory-item__back-icon" src={backArrow} alt="back" /></button></Link>{"Add New Inventory Item"}</h1>
             </div>
             <form className="inventory-item" onSubmit={handleSubmit}>
                 <section className="inventory-item__details-container">
@@ -110,20 +133,23 @@ export default function NewInventoryItem() {
                     <div className="inventory-item__form-group">
                         <div className="inventory-item__field-item">
                             <label className="inventory-item__label" htmlFor="itemName" >Item name</label>
-                            <input className="inventory-item__input" ref={itemName} type="text" name="itemName" id="itemName" placeholder='Item Name' />
+                            <input className={`inventory-item__input ${errors.itemName ? "inventory-item__input--error" : ""}`} ref={itemName} type="text" name="itemName" id="itemName" placeholder='Item Name' />
+                            {errors.itemName && <div className='inventory-item__error-label'><img className='inventory-item__error-icon' src={errorIcon} alt="error" />This field is required</div>}
                         </div>
                         <div className="inventory-item__field-item">
                             <label className="inventory-item__label" htmlFor="description">Description</label>
-                            <textarea ref={description} className="inventory-item__input inventory-item__input--textarea" name="description" id="description" placeholder='Enter description...' />
+                            <textarea ref={description} className={`inventory-item__input ${errors.description ? "inventory-item__input--error" : ""} inventory-item__input--textarea`} name="description" id="description" placeholder='Enter description...' />
+                            {errors.itemName && <div className='inventory-item__error-label'><img className='inventory-item__error-icon' src={errorIcon} alt="error" />This field is required</div>}
                         </div>
                         <div className="inventory-item__field-item">
                             <label className="inventory-item__label" htmlFor="category">Category</label>
-                            <select className="inventory-item__input inventory-item__input--select" name="warehouse" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                            <select className={`inventory-item__input ${errors.category ? "inventory-item__input--error" : ""} inventory-item__input--select`} name="warehouse" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
                                 <option value=""> Please Select </option>
                                 {categories.map(category => [
                                     <option key={category}>{category}</option>
                                 ])}
                             </select>
+                            {errors.itemName && <div className='inventory-item__error-label'><img className='inventory-item__error-icon' src={errorIcon} alt="error" />This field is required</div>}
                         </div>
                     </div>
                 </section>
@@ -142,34 +168,42 @@ export default function NewInventoryItem() {
                                     <input className="inventory-item__input inventory-item__input--radio" type="radio" value="Out of Stock" checked={status === 'Out of Stock'} onChange={handleStatusChange} />
                                     <label className="inventory-item__radio-label" htmlFor="statusOutOnStock">Out Of Stock</label>
                                 </div>
+
                             </div>
-                            {status === 'In Stock' && (
+                            {errors.itemName && <div className='inventory-item__error-label'><img className='inventory-item__error-icon' src={errorIcon} alt="error" />This field is required</div>}
+
+
+                        </div>
+                        {status === 'In Stock' && (
                             <div className=" inventory-item__quantity inventory-item__field-item inventory-item__field-item--radio ">
-                                <label className="inventory-item__label">Quantity<input className="inventory-item__input inventory-item__quantity" type="text" ref={quantity}></input></label>
+                                <label className="inventory-item__label">Quantity<input className="inventory-item__input inventory-item__quantity" type="number" ref={quantity} defaultValue={0}></input></label>
                             </div>
+
                         )}
+
+                        <div className="inventory-item__field-item">
+                            <label className="inventory-item__label" htmlFor="warehouse">Warehouse</label>
+                            <select className={`inventory-item__input ${errors.warehouse ? "inventory-item__input--error" : ""} inventory-item__input--select`} name="warehouse" value={selectedWarehouse?.id} onChange={handleWarehouseChange}>
+                                <option value="">Please Select</option>
+                                {warehouses.map((warehouse) => (
+                                    <option key={warehouse.id} value={warehouse.id}>{warehouse.warehouse_name}</option>
+                                ))}
+                            </select>
+                            {errors.itemName && <div className='inventory-item__error-label'><img className='inventory-item__error-icon' src={errorIcon} alt="error" />This field is required</div>}
                         </div>
                     </div>
 
-                    <div className="inventory-item__field-item">
-                        <label className="inventory-item__label" htmlFor="warehouse">Warehouse</label>
-                        <select className="inventory-item__input inventory-item__input--select" name="warehouse" value={selectedWarehouse?.id} onChange={handleWarehouseChange}>
-                            <option value="">Please Select</option>
-                            {warehouses.map((warehouse) => (
-                                <option key={warehouse.id} value={warehouse.id}>{warehouse.warehouse_name}</option>
-                            ))}
-                        </select>
-                    </div>
-              
-            </section>
 
-            <div className="inventory-item__actions">
-                <button className="inventory-item__button inventory-item__button--secondary" type="button">Cancel</button>
-                <button className="inventory-item__button inventory-item__button--primary" type="submit">Add Item</button>
-            </div>   
-        </form>
+
+                </section>
+
+                <div className="inventory-item__actions">
+                    <button className="inventory-item__button inventory-item__button--secondary" type="button">Cancel</button>
+                    <button className="inventory-item__button inventory-item__button--primary" type="submit">Add Item</button>
+                </div>
+            </form>
         </main >
-        
+
     )
 }
 
