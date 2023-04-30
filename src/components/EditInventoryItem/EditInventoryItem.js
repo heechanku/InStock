66e0139db1a26bfc08c68e04dc0f0455e2fe2e7c
Damien Switzer <damienswitzer@hotmail.com>
2023-04-29 18:10:45 -0700

    Add quantity field, correct warehouse value, add form validation

--- src/components/EditInventoryItem/EditInventoryItem.js
+++ src/components/EditInventoryItem/EditInventoryItem.js
@@ -8,9 +8,10 @@ const baseUrl = process.env.REACT_ALL_BASE_URL ?? "http://localhost:5050/api";
 
 function EditInventoryItem({ item }) {
     const navigate = useNavigate();
-    const [values, setValues] = useState({ itemName: item.item_name, description: item.description, category: item.category, status: item.status, warehouse: item.warehouse_id });
+    const [values, setValues] = useState({ itemName: item.item_name, description: item.description, category: item.category, status: item.status, warehouse: item.warehouse_id, quantity: item.quantity });
     const [warehouses, setWarehouses] = useState(null);
     const [categories, setCategories] = useState(null);
+    const [error,setError] = useState(false);
 
 
     //Onload get warehouses and categories
@@ -19,6 +20,7 @@ function EditInventoryItem({ item }) {
             .get(`${baseUrl}/warehouses`)
             .then(response => {
                 setWarehouses(response.data);
+                setValues({...values,"warehouse": response.data.find(warehouse => warehouse.warehouse_name===item.warehouse_name).id});
             })
             .catch(error => {
                 console.error(error);
@@ -41,6 +43,11 @@ function EditInventoryItem({ item }) {
     };
 
     const handleSave = () => {
+        if(!values.itemName || !values.description){
+            alert("Missing required fields.");
+            return;
+        }
+
         axios
             .put(`${baseUrl}/inventories/${item.id}`, {
                 "warehouse_id": values.warehouse,
@@ -48,7 +55,7 @@ function EditInventoryItem({ item }) {
                 "description": values.description,
                 "category": values.category,
                 "status": values.status,
-                "quantity": item.quantity
+                "quantity": values.quantity
             })
             .then(response => {
                 navigate(`/inventory/${item.id}`);
@@ -70,11 +77,11 @@ function EditInventoryItem({ item }) {
                 <div className="edit-inventory-item__form-group">
                     <div className="edit-inventory-item__field-item">
                         <label className="edit-inventory-item__label" htmlFor="itemName">Item name</label>
-                        <input className="edit-inventory-item__input" type="text" name="itemName" id="itemName" placeholder='Item Name' value={values.itemName} onChange={handleChange} />
+                        <input className="edit-inventory-item__input" type="text" name="itemName" id="itemName" required={true} placeholder='Item Name'  value={values.itemName} onChange={handleChange} />
                     </div>
                     <div className="edit-inventory-item__field-item">
                         <label className="edit-inventory-item__label" htmlFor="description">Description</label>
-                        <textarea className="edit-inventory-item__input edit-inventory-item__input--textarea" name="description" id="description" placeholder='Enter description...' value={values.description} onChange={handleChange} />
+                        <textarea className="edit-inventory-item__input edit-inventory-item__input--textarea" name="description" required={true} id="description" placeholder='Enter description...' value={values.description} onChange={handleChange} />
                     </div>
                     <div className="edit-inventory-item__field-item">
                         <label className="edit-inventory-item__label" htmlFor="category">Category</label>
@@ -91,21 +98,26 @@ function EditInventoryItem({ item }) {
                         <p className="edit-inventory-item__label">Status</p>
                         <div className="edit-inventory-item__radio-group">
                             <div className="edit-inventory-item__field-item edit-inventory-item__field-item--radio">
-                                <input className="edit-inventory-item__input edit-inventory-item__input--radio" type="radio" name="status" id="statusInStock" value="In Stock" checked={values.status === 'In Stock'} onChange={handleChange} />
-                                <label className="edit-inventory-item__radio-label" htmlFor="statusInStock">In Stock</label>
+                                <input className="edit-inventory-item__input edit-inventory-item__input--radio" type="radio" name="status" id="statusInStock" value="In Stock" checked={values.status === 'In Stock'} onChange={handleChange}/>
+                                <label className="edit-inventory-item__radio-label" htmlFor="statusInStock">In stock</label>
                             </div>
                             <div className="edit-inventory-item__field-item edit-inventory-item__field-item--radio">
                                 <input className="edit-inventory-item__input edit-inventory-item__input--radio" type="radio" name="status" id="statusOutOnStock" value="Out of Stock" checked={values.status === 'Out of Stock'} onChange={handleChange} />
-                                <label className="edit-inventory-item__radio-label" htmlFor="statusOutOnStock">Out Of Stock</label>
+                                <label className="edit-inventory-item__radio-label" htmlFor="statusOutOnStock">Out of stock</label>
                             </div>
                         </div>
                     </div>
+                    {values.status === 'In Stock' && <div className="edit-inventory-item__field-item">
+                        <label className="edit-inventory-item__label" htmlFor="warehouse">Quantity</label>
+                        <input className="edit-inventory-item__input" type="number" name="quantity" id="quantity" placeholder='' min="1" value={values.quantity} onChange={handleChange} />
+                    </div>}
                     <div className="edit-inventory-item__field-item">
                         <label className="edit-inventory-item__label" htmlFor="warehouse">Warehouse</label>
-                        <select className="edit-inventory-item__input edit-inventory-item__input--select" name="warehouse" value={values.warehouse_id} onChange={handleChange} >
+                        <select className="edit-inventory-item__input edit-inventory-item__input--select" name="warehouse" value={values.warehouse} onChange={handleChange} >
                             {warehouses && warehouses.map(warehouse => <option key={warehouse.id} value={warehouse.id}>{warehouse.warehouse_name}</option>)}
                         </select>
                     </div>
+
 
                 </div>
 
